52158b8ef0da85581c379ceca7f9ce85c1cf5056
Simeon Naydenov <moniNaydenov@gmail.com>
2025-02-06 12:10:28 +0200

    improve process kill - try yet another way

--- ffmpeg_manager.py
+++ ffmpeg_manager.py
@@ -17,34 +17,35 @@ class BackgroundProcessManager:
         self.input_stream = os.environ.get("ENV_INPUT_STREAM")
         self.output_stream = os.environ.get("ENV_OUTPUT_STREAM")     
     def _run_command(self):
-        """Runs the command in the background."""
         command = self.command_template % {
             'ENV_INPUT_STREAM': self.input_stream,
             'ENV_OUTPUT_STREAM': self.output_stream
         }
-        print(f"Running command: {command}")
+        print(f"Running command: {command}", flush=True)
         try:
-            self.process = subprocess.Popen(command, shell=True, preexec_fn=os.setsid)
-            print(f"Process started with PID: {self.process.pid}", flush=True)
+           self.process = subprocess.Popen(command, shell=False, preexec_fn=os.setsid) #shell=False is important
+           print(f"Process started with PID: {self.process.pid}", flush=True)
         except Exception as e:
             print(f"Error starting process: {e}", flush=True)
 
     def _stop_command(self):
-        if self.process and self.process.poll() is None: #Check if the process is running
+        if self.process and self.process.poll() is None:
             print(f"Stopping process with PID: {self.process.pid}", flush=True)
             try:
-                self.process.terminate() #Try graceful termination first
+                os.killpg(os.getpgid(self.process.pid), signal.SIGTERM) #Try to kill the group first
                 self.process.wait(timeout=10)  # Wait for it to finish
                 print("Process stopped gracefully.", flush=True)
 
             except subprocess.TimeoutExpired:
                 print("Process did not terminate gracefully. Using SIGKILL.", flush=True)
                 try:
-                    self.process.kill()  # Forcefully kill the process
+                    os.killpg(os.getpgid(self.process.pid), signal.SIGKILL)  # Forcefully kill the process group
                     self.process.wait(timeout=5) #Wait for kill to happen
-                    print("Process killed.", flush=True)
+                    print("Process group killed.", flush=True)
                 except Exception as e:
                     print(f"Error killing process: {e}", flush=True)
+            except ProcessLookupError: #Handle the case when group is already killed.
+                print("Process group already killed.", flush=True)
             except Exception as e:
                 print(f"Error stopping process: {e}", flush=True)
             finally:
